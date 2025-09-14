6a0230115e4808c523585966f7006eeaebb45306
Iheb Chebbi <iheb.chebbi@lcieducation.net>
2025-09-14 15:28:58 +0100

    Changed to neon database

--- Migrations/20250914135800_InitialCreate.cs
+++ Migrations/20250914135800_InitialCreate.cs
@@ -6,7 +6,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FlowServiceBackend.Migrations
 {
     /// <inheritdoc />
-        public partial class PostgreSQLInitialSetup : Migration    {
+        public partial class InitialCreate : Migration    {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
