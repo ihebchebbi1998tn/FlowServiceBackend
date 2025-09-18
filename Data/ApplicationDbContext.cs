1a070e965341c49cf98e7bc9e89ab08b05e6ef97
Iheb Chebbi <141638922+ihebchebbi1998tn@users.noreply.github.com>
2025-09-18 20:32:09 +0100

    de

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -93,7 +93,10 @@ namespace MyApi.Data
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
                 entity.Property(e => e.Description).HasMaxLength(1000);
-                entity.Property(e => e.Price).HasColumnType("decimal(18,2)");
+                entity.Property(e => e.CostPrice).HasColumnType("decimal(18,2)");
+                entity.Property(e => e.SellPrice).HasColumnType("decimal(18,2)");
+                entity.Property(e => e.BasePrice).HasColumnType("decimal(18,2)");
+                entity.Property(e => e.HourlyRate).HasColumnType("decimal(18,2)");
             });
         }
 
