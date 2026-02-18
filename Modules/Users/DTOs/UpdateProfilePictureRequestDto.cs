e7759206cdce12b409a9423fd9b896b8f8f69592
iheb chebbi <iheb.chebbi@lcieducation.net>
2026-02-18 13:03:43 +0100

    ad

--- Modules/Users/DTOs/UpdateProfilePictureRequestDto.cs
+++ Modules/Users/DTOs/UpdateProfilePictureRequestDto.cs
@@ -1,13 +1,1 @@
-using System.Text.Json.Serialization;
-
-namespace MyApi.Modules.Users.DTOs
-{
-    /// <summary>
-    /// Dedicated DTO for updating a regular user's profile picture URL.
-    /// </summary>
-    public class UpdateProfilePictureRequestDto
-    {
-        [JsonPropertyName("profilePictureUrl")]
-        public string? ProfilePictureUrl { get; set; }
-    }
-}
+// This file is deprecated - use MyApi.Modules.Auth.DTOs.UpdateProfilePictureRequestDto instead
